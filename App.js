3e4111404f987422942a489d29c13583d59a33db
Brandon Davenport <davenportb40@gmail.com>
2018-07-17 00:24:05 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -15,17 +15,12 @@ import MixedNavigators from './src/Navigation/MixedNavigators';
 
 // ---------------- APP SHEET --------------------------//
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component {
   render() {
     return (
       <MixedNavigators />
+
     );
   }
 }
